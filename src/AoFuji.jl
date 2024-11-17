8f7b5e9a2d37916b506c2bad979a8fe1746c7347
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-17 16:28:46 +0900

    Split file

--- src/AoFuji.jl
+++ src/AoFuji.jl
@@ -1,5 +1,7 @@
 module AoFuji
 
+include("graph.jl")
+
 """
     rle(s::String)
 
@@ -62,35 +64,4 @@ function bitsearch(array::Vector{})
 
     return s
 end
-
-
-module Graph
-"""
-    bfs(graph::Vector{}, start::Int)
-
-Perform  breadth first search for adjacency list.
-"""
-function bfs(graph::Vector{}, start::Int)
-    nexts = [start]
-
-    seen = fill(-1, length(graph))
-    seen[start] = 0
-
-    while !isempty(nexts)
-        target_vertex = popfirst!(nexts)
-
-        for vertex ∈ graph[target_vertex]
-            if seen[vertex] ≠ -1
-                continue
-            end
-
-            push!(nexts, vertex)
-            seen[vertex] = seen[target_vertex] + 1
-        end
-    end
-
-    return seen
-end
-end
-
 end
