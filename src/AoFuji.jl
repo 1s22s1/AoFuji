8fc15a4b51a25696962cc74ce9bef82de27c3e8a
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-17 16:25:04 +0900

    Add Graph module

--- src/AoFuji.jl
+++ src/AoFuji.jl
@@ -25,33 +25,6 @@ function rle(s::String)
 end
 
 """
-    bfs(graph::Vector{}, start::Int)
-
-Perform  breadth first search for adjacency list.
-"""
-function bfs(graph::Vector{}, start::Int)
-    nexts = [start]
-
-    seen = fill(-1, length(graph))
-    seen[start] = 0
-
-    while !isempty(nexts)
-        target_vertex = popfirst!(nexts)
-
-        for vertex ∈ graph[target_vertex]
-            if seen[vertex] ≠ -1
-                continue
-            end
-
-            push!(nexts, vertex)
-            seen[vertex] = seen[target_vertex] + 1
-        end
-    end
-
-    return seen
-end
-
-"""
     tally(array::Union{Vector{Int}, String})
 
 Returns a hash containing the counts of equal elements.
@@ -88,6 +61,36 @@ function bitsearch(array::Vector{})
     end
 
     return s
+end
+
+
+module Graph
+"""
+    bfs(graph::Vector{}, start::Int)
+
+Perform  breadth first search for adjacency list.
+"""
+function bfs(graph::Vector{}, start::Int)
+    nexts = [start]
+
+    seen = fill(-1, length(graph))
+    seen[start] = 0
+
+    while !isempty(nexts)
+        target_vertex = popfirst!(nexts)
+
+        for vertex ∈ graph[target_vertex]
+            if seen[vertex] ≠ -1
+                continue
+            end
+
+            push!(nexts, vertex)
+            seen[vertex] = seen[target_vertex] + 1
+        end
+    end
+
+    return seen
+end
 end
 
 end
--- test/graph.jl
+++ test/graph.jl
@@ -2,8 +2,8 @@ using Test
 
 @testset "graph.jl" begin
     @testset "bfs" begin
-        @test AoFuji.bfs([[2], [3], [1]], 1) == [0, 1, 2]
-        @test AoFuji.bfs([[2, 3], [3, 1], [1, 2]], 1) == [0, 1, 1]
-        @test AoFuji.bfs([[2], [], [2]], 1) == [0, 1, -1]
+        @test AoFuji.Graph.bfs([[2], [3], [1]], 1) == [0, 1, 2]
+        @test AoFuji.Graph.bfs([[2, 3], [3, 1], [1, 2]], 1) == [0, 1, 1]
+        @test AoFuji.Graph.bfs([[2], [], [2]], 1) == [0, 1, -1]
     end
 end
