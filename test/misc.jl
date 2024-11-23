424bce107134ba4caac8aa83b52458be6d173e89
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-23 14:36:48 +0900

    Add searchsorted

--- test/misc.jl
+++ test/misc.jl
@@ -34,4 +34,16 @@ using Printf
     @testset "原点からの距離" begin
         @test hypot(1, 2) == 2.23606797749979
     end
+
+    @testset "3以上の要素の最初の添字を取得" begin
+        @test searchsortedfirst([1, 2, 3, 4, 5, 6], 3) == 3
+        @test searchsortedfirst([1, 2, 2, 3, 3, 3], 3) == 4
+        @test searchsortedfirst([1, 2], 3) == 3
+    end
+
+    @testset "3未満の要素の最後の要素の添字を取得" begin
+        @test searchsortedlast([1, 2, 3, 4, 5, 6], 3) == 3
+        @test searchsortedlast([1, 2, 2, 3, 3, 3], 3) == 6
+        @test searchsortedlast([1, 2], 3) == 2
+    end
 end
