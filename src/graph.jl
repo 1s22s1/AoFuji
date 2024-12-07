3cf8eaaa4b80a1f8ba8c8cc9275539e6b5fc6d6e
1s22s1 <1s22s1@users.noreply.github.com>
2024-12-08 08:15:07 +0900

    Define maze_bfs

--- src/graph.jl
+++ src/graph.jl
@@ -25,4 +25,28 @@ function bfs(graph::Vector{}, start::Int)::Vector{}
 
     return seen
 end
+
+function maze_bfs(maze, h, w)
+    seen = fill(-1, 3, 4)
+
+    nexts = [[1, 1]]
+    seen[1, 1] = 0
+
+    while !isempty(nexts)
+        cur_pos = popfirst!(nexts)
+
+        for (x, y) ∈ [[1, 0], [-1, 0], [0, -1], [0, 1]]
+            next_x = cur_pos[begin] + x
+            next_y = cur_pos[end] + y
+
+            if 1 ≤ next_x ≤ h && 1 ≤ next_y ≤ w && maze[next_x][next_y] == "." && seen[next_x, next_y] == -1
+                push!(nexts, [next_x, next_y])
+
+                seen[next_x, next_y] = seen[cur_pos[begin], cur_pos[end]] + 1
+            end
+        end
+    end
+
+    return seen
+end
 end
--- test/graph.jl
+++ test/graph.jl
@@ -6,4 +6,10 @@ using Test
         @test AoFuji.Graph.bfs([[2, 3], [3, 1], [1, 2]], 1) == [0, 1, 1]
         @test AoFuji.Graph.bfs([[2], [], [2]], 1) == [0, 1, -1]
     end
+
+    @testset "maze_bfs" begin
+        maze = [[".", ".", ".", "."], ["#", ".", ".", "."], [".", "#", ".", "#"]]
+
+        @test AoFuji.Graph.maze_bfs(maze, 3, 4) == [0 1 2 3; -1 2 3 4; -1 -1 4 -1]
+    end
 end
